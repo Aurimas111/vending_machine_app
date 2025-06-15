258d6df7f3bb155c8fee0d7272da3a8c6923a1c3
Aurimas Čivas <67438154+Aurimas111@users.noreply.github.com>
2025-06-15 23:59:35 +0300

    api url updated

--- frontend/src/services/refundsService.js
+++ frontend/src/services/refundsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/refunds/';
+const API_URL = 'https://iamaurimas.xyz/api/minter/';
 
 class refundsService {
 
--- frontend/src/services/transactionsService.js
+++ frontend/src/services/transactionsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/minter/';
+const API_URL = 'https://iamaurimas.xyz/api/minter/';
 
 class transactionsService {
 
